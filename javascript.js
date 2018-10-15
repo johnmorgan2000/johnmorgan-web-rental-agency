5d1b1fd17a3463ec673e4998f127426e7bdce4cf
basecamp <jmorgan@basecampcodingacademy.org>
2018-10-15 09:30:39 -0500

    Added a function to allow me to select an item to put in cart, the adding to cart action stills needs to be implemented

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const cart = [];
+
+// Adds the product from the inventory to page
 function renderProduct(inv) {
     var source = document.getElementById("item").innerHTML;
     var template = Handlebars.compile(source);
@@ -7,4 +10,21 @@ function renderProduct(inv) {
     document.getElementById("product").insertAdjacentHTML("beforeend", html);
 }
 
+// Adds eventlisters to the buttons to add it to cart
+function selectItem() {
+    var products = document.querySelectorAll(".product");
+    for (const i of products) {
+        let btn = i.querySelector("button");
+        btn.addEventListener("click", () => {
+            console.log(i.querySelector(".name").innerText);
+        });
+    }
+}
+
+// Takes the product from inventory and add it to cart
+function addToCart(cart, inv) {
+    cart.push();
+}
+
 renderProduct(INVENTORY);
+selectItem();
