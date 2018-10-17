915abfd5bc32f8e48908d748d79db2d2e5a24153
basecamp <jmorgan@basecampcodingacademy.org>
2018-10-17 08:38:39 -0500

    Updated the items in inventory

--- inventory.js
+++ inventory.js
@@ -1,50 +1,50 @@
 const INVENTORY = [
     {
-        name: "Item",
+        name: "Killer Spin Paddle",
         category: ["Popular"],
-        imageURL: "images/placeholder.png",
-        description: "Description",
+        imageURL: "images/KillerSpin.jpeg",
+        description: "A Killer Spin quality professional paddle.",
+        instock: 8,
+        price: 15
+    },
+    {
+        name: "ESPN Table",
+        category: ["Popular"],
+        imageURL: "images/espn_table.jpeg",
+        description: "A ESPN tournament sized folding table",
+        instock: 3,
+        price: 69.99
+    },
+    {
+        name: "MD sports Kids Table",
+        category: ["Popular"],
+        imageURL: "images/kids_table.jpeg",
+        description: "A MD brand kids table tennis table",
         instock: 10,
         price: 10
     },
     {
-        name: "Item2",
+        name: "STIGA Flow Paddle",
         category: ["Popular"],
-        imageURL: "images/placeholder.png",
-        description: "Description",
+        imageURL: "images/stiga.jpeg",
+        description: "A STIGA Flow paddle",
         instock: 10,
-        price: 10
+        price: 5.65
     },
     {
-        name: "Item3",
+        name: "Jim's lucky ball",
         category: ["Popular"],
-        imageURL: "images/placeholder.png",
-        description: "Description",
-        instock: 10,
-        price: 10
+        imageURL: "images/lucky_ball.jpeg",
+        description: "A lucky ball that once belong to Jim",
+        instock: 0,
+        price: 2
     },
     {
-        name: "Item4",
+        name: "Super magic ball",
         category: ["Popular"],
         imageURL: "images/placeholder.png",
         description: "Description",
-        instock: 10,
-        price: 10
-    },
-    {
-        name: "Item5",
-        category: ["Popular"],
-        imageURL: "images/placeholder.png",
-        description: "Description",
-        instock: 10,
-        price: 10
-    },
-    {
-        name: "Item6",
-        category: ["Popular"],
-        imageURL: "images/placeholder.png",
-        description: "Description",
-        instock: 10,
-        price: 10
+        instock: 1,
+        price: 100000
     }
 ];
